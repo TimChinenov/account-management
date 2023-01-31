6eb295263c9045127b108105b83f42af1aa6aeca
Tim Chinenov <tim.chinenov@gmail.com>
2023-01-30 21:02:03 -0800

    Added integration test for creating a user

--- main.go
+++ main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"example/account-management/controllers"
-
-	"github.com/gin-gonic/gin"
+	"example/account-management/router"
 )
 
 func main() {
-	router := setupRouter()
+	router := router.SetupRouter()
 
 	router.Run("localhost:8080")
-}
-
-func setupRouter() *gin.Engine {
-	router := gin.Default()
-	router.POST("/login", controllers.LoginUser)
-	router.POST("/users", controllers.CreateUser)
-
-	return router
 }
---
+++ tests/user_test.go
@@ -0,0 +1,36 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"example/account-management/models"
+	"example/account-management/router"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateUser(t *testing.T) {
+	router := router.SetupRouter()
+
+	w := httptest.NewRecorder()
+
+	body := []byte(`{"username": "timmy", "password": "test"}`)
+	bodyReader := bytes.NewReader(body)
+	req, _ := http.NewRequest("POST", "/users", bodyReader)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, 201, w.Code)
+
+	// Convert the JSON response to a map
+	var response models.UserResponse
+	json.Unmarshal([]byte(w.Body.String()), &response)
+	// Grab the value & whether or not it exists
+	username := response.Username
+	assert.Equal(t, "timmy", username)
+
+	score := response.Score
+	assert.Equal(t, 0, score)
+}
